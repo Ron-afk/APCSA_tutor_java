be15124fd54da6cdeb12457cb4c7740930c76775
Ron-afk <robbie616318821@gmail.com>
2023-10-07 09:55:34 -0700

    push

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+//        example 1.1
+//        int a = 0;
+//        int b = 1;
+//        int c = a+b;
+//        System.out.println(c);
+        // example 1.2
+//        int a;
+//        int b,c,d;
+//        E1_1();
     }
+
+    public static void E1_1(){
+        //todo: Declare two int variables a and b
+
+        //todo: assign 12 to a, and 349 to b
+
+        //todo: compare a and b and store the result into a boolean variable c
+
+//        System.out.println(c);
+    }
+
+
 }
